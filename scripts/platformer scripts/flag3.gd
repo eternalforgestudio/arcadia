61a1ff82976b7ceaf92618c8dedbf4714e69cd3c
Aly Al-Gendy <aly.algendy.2008@gmail.com>
2026-09-21 02:34:19 +0300

    finished platformer minigame

--- scripts/platformer scripts/flag3.gd
+++ scripts/platformer scripts/flag3.gd
@@ -5,4 +5,4 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
-		get_tree().call_deferred("Change_scene_to_path",level_four_path)
+		get_tree().call_deferred("change_scene_to_file",level_four_path)
--- scripts/platformer scripts/flag4.gd
+++ scripts/platformer scripts/flag4.gd
@@ -6,4 +6,4 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
-		get_tree().call_deferred("change_scene_to_path", level_five_path)
+		get_tree().call_deferred("change_scene_to_file", level_five_path)
